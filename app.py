af8c7183273f8d2d0073634dd6b7f14dc9479580
Simon Boylan <siboy73@gmail.com>
2024-03-19 22:02:56 +0000

    Add join date to registration

--- app.py
+++ app.py
@@ -68,7 +68,8 @@ def register():
             "last_name": request.form.get("last_name"),
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password")),
-            "email": request.form.get("email")
+            "email": request.form.get("email"),
+            "join_date": datetime.utcnow() 
         }
         mongo.db.users.insert_one(register)
 
