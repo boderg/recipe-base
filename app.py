c2b108dcd480c850d22521f8873047aedfbd4af7
Simon Boylan <siboy73@gmail.com>
2024-03-21 13:14:26 +0000

    Edit join date to display correctly

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def register():
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password")),
             "email": request.form.get("email"),
-            "join_date": datetime.utcnow() 
+            "join_date": datetime.now().strftime("%d-%m-%Y at %H:%M")
         }
         mongo.db.users.insert_one(register)
 
